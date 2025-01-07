73372043573f74e52e71134ebf889f1cd34ebf33
JangJinWoo <aktsksqkq@gmail.com>
2025-01-07 15:57:32 +0900

    필수과제 완

---
+++ MyActor.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class PROJECTS_4THWEEK_1ST_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+	void RandomMove(FVector2D&);
+	void DisplayMove(FVector2D, FVector2D);
+	void Step(FVector2D&, FVector2D);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
