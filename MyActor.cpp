96236aae73a542214472e36948a3a640fc498701
JangJinWoo <aktsksqkq@gmail.com>
2025-01-07 16:25:54 +0900

    아랫꺼 잘못올림 - 요게 필수완

--- MyActor.cpp
+++ MyActor.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MyActor.h"
@@ -11,9 +11,37 @@ AMyActor::AMyActor()
 
 }
 
+void AMyActor::RandomMove(FVector2D& _Move)
+{
+	_Move.X = FMath::RandRange(-1, 1);
+	_Move.Y = FMath::RandRange(-1, 1);
+}
+
+void AMyActor::DisplayMove(FVector2D _Coor, FVector2D _Move)
+{
+	UE_LOG(LogTemp, Log, TEXT("현재 좌표 : (%.0f, %.0f) +++ 이동 좌표(%.0f, %.0f) === 목표 좌표 : (%.0f, %.0f)"), _Coor.X, _Coor.Y, _Move.X, _Move.Y, _Coor.X + _Move.X, _Coor.Y + _Move.Y);
+}
+
+void AMyActor::Step(FVector2D& _Coor, FVector2D _Move)
+{
+	_Coor.X += _Move.X;
+	_Coor.Y += _Move.Y;
+}
+
 // Called when the game starts or when spawned
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
+	FVector2D MyCoordinate = FVector2D::ZeroVector;
+	FVector2D MoveVector;
+
+	UE_LOG(LogTemp, Warning, TEXT("Hello World!"));
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		RandomMove(MoveVector);
+		DisplayMove(MyCoordinate, MoveVector);
+		Step(MyCoordinate, MoveVector);
+	}
 }
 
